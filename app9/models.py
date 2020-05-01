3b2ce4e303e467fa4e10b7dd20fb0295ab6fe9dd
Johanes Steven <johanessteven19@gmail.com>
2020-05-01 19:38:25 +0700

    [REFACTOR] Fix Search Box

--- app9/models.py
+++ app9/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class TopLiked(models.Model):
+    title = models.CharField(max_length = 256)
+    amount = models.IntegerField(default = 0)
+
+    class Meta:
+        ordering = ['-amount']
+
+    def __str__(self):
+        return "%s - %d" % (self.title, self.amount)
--- app9/views.py
+++ app9/views.py
@@ -1,5 +1,32 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, JsonResponse
+from .models import TopLiked
+import json
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
+
+@csrf_exempt
 def home(request):
-    return render(request, 'index.html')
+    if request.method == 'POST':
+        title = request.POST['title']
+        amount = request.POST['amount']
+
+        TopLiked.objects.create(
+            title = title,
+            amount = amount
+        )
+    if TopLiked.objects.all().count() < 5:
+        data0 = ""
+        data1 = ""
+        data2 = ""
+        data3 = ""
+        data4 = ""
+    else:
+        data0 = TopLiked.objects.all()[0]
+        data1 = TopLiked.objects.all()[1]
+        data2 = TopLiked.objects.all()[2]
+        data3 = TopLiked.objects.all()[3]
+        data4 = TopLiked.objects.all()[4]
+    show = {'data0': data0, 'data1': data1, 'data2': data2, 'data3': data3, 'data4': data4,}
+    return render(request, 'index.html', show)
+
